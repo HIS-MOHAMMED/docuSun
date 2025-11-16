4873c59a16a7ad9ea5db697c714e1ab732815e84
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-11-16 22:17:41 +0300

    feat: add pdf loader feature and its unit tests (#15)
    * feat: add load pdf files feature
    
    * test: add unit tests for load_pdf function on loaders

---
+++ engine/ingestion/loaders.py
@@ -0,0 +1,47 @@
+import pymupdf
+from langchain.docstore.document import Document
+
+def load_pdf(files):
+    """
+    Loads documents from PDF files using PyMuPDF
+
+    Parameters:
+    - files : A String representing a single file path or a list of strings representing multiple file paths
+    
+    Returns:
+    - A list of Documens objects loaded from the provided pdf files
+    
+    Raises:
+    - FileNotFoundException: If any of provided file pahts do not exists.
+    - Exception: For any other issues encountered during file loading
+    """
+    if not isinstance(files, list):
+        files = [files]
+    documents = []
+    for file_path in files:
+        try:
+            # Open the PDF file
+            doc = pymupdf.open(file_path)
+            
+            # Extract text from each page
+            text = ""
+            for page_num in range(len(doc)):
+                page = doc.load_page(page_num)
+                text += page.get_text("text")
+
+            # Create a Document object 
+            document = Document(
+                page_content=text,
+                metadata={"source": file_path}
+            )
+
+            # Add the Document object to documents list
+            documents.append(document)      
+
+        except FileNotFoundError as exception:
+            print(f"file not found: {exception.filename}")
+            raise
+        except Exception as exception:
+            print(f"An error occurred while loading {file_path}: {e}")
+            raise 
+    return documents       
