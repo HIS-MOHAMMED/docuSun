58971660389f0e09b1bb4c1f2c479c7fb29b0c18
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-12-04 22:13:43 +0300

    update sources module and its unit tests to accept directory path as parameter (#20)
    * feat: update sources module to accept directory path as input
    
    * test: update unit tests to cover path parameter

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,20 +1,31 @@
 import os
+import shutil
+import pymupdf
 from engine.ingestion.sources import discover_files
+
+def create_temp_pdf(path):
+    # Create a very small empty pdf with multiple pages
+    doc = pymupdf.open()
+
+    page1 = doc.new_page()
+    page1.insert_text((72,72),"")
+
+    page2 = doc.new_page()
+    page2.insert_text((72,72), "")
+
+    doc.save(path)
+    doc.close()
 
 def test_discover_files(tmp_path):
     # Arrange : create temporary data directory and files
-    data_dir  = tmp_path/'data'
+    data_dir  = tmp_path/'tests'
     os.mkdir(data_dir)
-    (data_dir / 'a.pdf').touch()
-    (data_dir / 'b.txt').touch()
-    (data_dir / 'c.pdf').touch()
 
-    # Act: temporarily swich to that folder and run function
-    cwd = os.getcwd()
-    os.chdir(tmp_path)
-    result = discover_files()
-    os.chdir(cwd)
+    create_temp_pdf(data_dir/"sample.pdf")
+    
+    # Act: run function the function
+    result = discover_files(data_dir)
 
     # Assert : only PDF file are detected
-    expected = [f'data/a.pdf',f'data/c.pdf']
-    assert sorted(result) == sorted(expected)
+    assert len(result) == 1
+    assert result[0].endswith('tests/sample.pdf')
